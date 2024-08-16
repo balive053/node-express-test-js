2a0e2fa11a5f989dafd0887d11dea77718013a9a
blake livermore <blake.livermore@gmail.com>
2024-08-16 23:15:29 +0200

    added get requests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 
 
 const app = express();
@@ -7,4 +7,30 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Running on port: ${PORT}`)
+})
+
+const mockUsers = [
+    {"id":1, "name":'Bob'},
+    {"id":2, "name":'Charlotte'},
+    {"id":3, "name":'William'}
+]
+
+app.get('/api/users', (request, response) => {
+    console.log(mockUsers);
+    response.send(mockUsers)
+})
+
+//get route params as input
+app.get('/api/users/:id', (request, responce) => {
+    console.log(request.params);
+    const parsedId = parseInt(request.params.id) 
+    if (isNaN(parsedId))
+        return responce.status(400).send("Bad request. Invalid user ID.")
+    
+    const userResponse = mockUsers.find((user) => user.id === parsedId)
+    // if user doesn't exist
+    if (!userResponse)
+        return responce.sendStatus(404)
+    else
+        return responce.send(userResponse)
 })
